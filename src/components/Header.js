ba41bb4069c7f961c4cdcb7e956535218e9f53b5
Travis Miller <travis@underdogsystems.com>
2019-09-26 10:20:36 -0500

    Link logo to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import LanguageButton from './LanguageButton'
 
 const Header = ({ logo }) => {
   return (
     <header className="flex items-center">
       <div className="flex-1 text-left">
-        <img alt="" src={logo} />
+        <Link to="/"><img alt="" src={logo} /></Link>
       </div>
       <div className="flex-1 text-right text-xs md:text-base">
         <LanguageButton code="en" name="English" />
